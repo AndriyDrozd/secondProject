052120fa5fe84bca0027be19c12ed60189651c46
AndriyDrozd <kraken159951@gmail.com>
2018-10-30 16:13:40 +0200

    Added ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,13 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 
 export class RecipeService {
     recipeSelected =  new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test Recipe', 'This is simply a test','https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+        new Recipe('A test Recipe', 'This is simply a test','https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',[new Ingredient('Meat',1), new Ingredient('French Fries',20)])
       ];
 
       getRecipes() {
