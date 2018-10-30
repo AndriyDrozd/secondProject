f44622aae7a9949f791a06d8745a877258d10bc7
AndriyDrozd <kraken159951@gmail.com>
2018-10-30 16:44:21 +0200

    Passed Ingredients from Recipes to the Shopping List

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,9 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
-
+@Injectable()
 export class RecipeService {
     recipeSelected =  new EventEmitter<Recipe>();
 
@@ -10,8 +11,14 @@ export class RecipeService {
         new Recipe('A test Recipe', 'This is simply a test','https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',[new Ingredient('Meat',1), new Ingredient('French Fries',20)])
       ];
 
+      constructor(private slService: ShoppingListService) {}
+
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
       }
 }
 
