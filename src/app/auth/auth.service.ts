46f3a1fdc49fb97306c60ea052f36b33c7e0fa9c
AndriyDrozd <kraken159951@gmail.com>
2018-11-09 14:45:54 +0200

    Signed users up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,10 @@
-
+import * as firebase from 'firebase/app';
 
 export class AuthService {
     singupUser(email: string, password: string) {
-        
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(
+            error => console.log(error)
+        )
     }
 }
