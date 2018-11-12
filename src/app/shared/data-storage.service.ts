dee7848646c43069ef1aa0c63d5954cdb8846812
AndriyDrozd <kraken159951@gmail.com>
2018-11-12 16:24:50 +0200

    Used HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
-import { Http, Response } from '@angular/http';
 import { Recipe } from "../recipes/recipe.model";
 import { map } from 'rxjs/operators';
 import { AuthService } from "../auth/auth.service";
+import { HttpClient } from "@angular/common/http";
 
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http,
+    constructor(private httpClient: HttpClient,
         private recipeService: RecipeService,
         private authService: AuthService) {}
 
     storeRecipes() {
        const token = this.authService.getToken();
-       return this.http.put('https://ng-recipe-book-ce164.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+       return this.httpClient.put('https://ng-recipe-book-ce164.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
         const token = this.authService.getToken();
-        this.http.get('https://ng-recipe-book-ce164.firebaseio.com/recipes.json?auth=' + token)
+        this.httpClient.get<Recipe[]>('https://ng-recipe-book-ce164.firebaseio.com/recipes.json?auth=' + token)
         .pipe(map(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
+            (recipes) => {
                 for(let recipe of recipes){
                     if(!recipe['ingredients']) {
                         console.log(recipe);
