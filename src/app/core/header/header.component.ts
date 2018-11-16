76bb7a74d552dafdfb9186c7646d32273fe00880
AndriyDrozd <kraken159951@gmail.com>
2018-11-16 15:24:57 +0200

    Used Authentication

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +13,14 @@ import { HttpEvent, HttpEventType } from '@angular/common/http';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>;
  
   constructor(private dataStorageService: DataStorageService,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.authState = this.store.select('auth');
   }
 
 
