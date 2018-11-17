4909e4bae8d3a8529d2b60eea50c401cc0918b43
AndriyDrozd <kraken159951@gmail.com>
2018-11-17 10:16:37 +0200

    Handled Logout via NgRx

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 import { map } from 'rxjs/operators';
-import { AuthService } from "../auth/auth.service";
 import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from "@angular/common/http";
 
 
 @Injectable()
 export class DataStorageService {
     constructor(private httpClient: HttpClient,
-        private recipeService: RecipeService,
-        private authService: AuthService) {}
+        private recipeService: RecipeService,) {}
 
     storeRecipes() {
        
