25b0e738fb65365ed8b4d5c3e1b9d90738985a34
AndriyDrozd <kraken159951@gmail.com>
2018-11-18 10:23:05 +0200

    Recipes Side Effects - Storing Recipes on Server

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,16 +1,19 @@
 import { Effect, Actions } from "@ngrx/effects";
 
 import * as RecipeActions from '../store/recipe.actions';
-import { switchMap, map } from "rxjs/operators";
-import { HttpClient } from "@angular/common/http";
+import { switchMap, map, withLatestFrom } from "rxjs/operators";
+import { HttpClient, HttpRequest } from "@angular/common/http";
 import { Recipe } from "../recipe.model";
 import { Injectable } from "@angular/core";
+import * as fromRecipe from '../store/recipe.reducers';
+import { Store } from "@ngrx/store";
 
 @Injectable()
 export class RecipeEffects {
 
     constructor(private actions$: Actions,
-        private httpClient: HttpClient) {}
+        private httpClient: HttpClient,
+        private store: Store<fromRecipe.FeatureState>) {}
 
     @Effect()
     recipeFetch = this.actions$
@@ -37,6 +40,17 @@ export class RecipeEffects {
             
         }
     ));
+
+
+    @Effect({dispatch: false})
+    recipeStore = this.actions$
+    .ofType(RecipeActions.STORE_RECIPES)
+    .pipe(withLatestFrom(this.store.select('recipes'))
+    ,switchMap(([action, state]) => {
+        const req = new HttpRequest('PUT', 'https://ng-recipe-book-ce164.firebaseio.com/recipes.json',
+        state.recipes, {reportProgress: true})
+        return this.httpClient.request(req);
+    }));
 
     
 }
