bae764d0d0669a119a2d2e3dc56b01b55844b3bb
AndriyDrozd <kraken159951@gmail.com>
2018-11-18 10:31:42 +0200

    Made some fixes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -34,11 +34,6 @@ export class RecipeEditComponent implements OnInit {
     );
   }
   onSubmit() {
-    // const newRecipe = new Recipe(
-    //   this.recipeForm.value['name'], 
-    //   this.recipeForm.value['description'],
-    //   this.recipeForm.value['imagePath'],
-    //   this.recipeForm.value['ingredients']);
     if(this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeForm.value);
       this.store.dispatch(new RecipeActions.UpdateRecipe({index: this.id, updateRecipe: this.recipeForm.value}));
